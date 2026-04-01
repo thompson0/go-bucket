fefaa58f5535d33b200de7bf2220e89c0ddf3f36
thompson0 <thompsonjooj@gmail.com>
2026-04-01 00:19:31 -0300

    feat:brute new method

--- buckets/brute.go
+++ buckets/brute.go
@@ -5,9 +5,10 @@ import (
 	"fmt"
 	"net/url"
 	"os"
+	"strconv"
 	"sync"
-	"time"
 	"strings"
+	"time"
 )
 
 func Brute(name string, stopOnFound bool, wordlistPath string, numThreads int, timeout int, outputFile string, debug bool) {
@@ -82,6 +83,7 @@ type BruteContext struct {
 	StopOnFound bool
 	OutFile     *os.File
 	Stop        chan struct{}
+	StopOnce    sync.Once
 	Name        string
 	Debug       bool
 }
@@ -105,35 +107,142 @@ func normalizeBucketName(input string) string {
 	}
 
 	input = strings.TrimSuffix(input, ".s3.amazonaws.com")
-	return input
+	return strings.ToLower(input)
 }
 
+func sanitizeBucketPart(input string) string {
+	input = strings.ToLower(strings.TrimSpace(input))
+	if input == "" {
+		return ""
+	}
 
+	var b strings.Builder
+	b.Grow(len(input))
+	lastSep := false
 
-func worker(wg *sync.WaitGroup, jobs <-chan string, ctx *BruteContext) {
-	defer wg.Done()
+	for _, r := range input {
+		isLetter := r >= 'a' && r <= 'z'
+		isNumber := r >= '0' && r <= '9'
+		if isLetter || isNumber {
+			b.WriteRune(r)
+			lastSep = false
+			continue
+		}
 
-	for word := range jobs {
-		select {
-		case <-ctx.Stop:
-			return
-		default:
-			bucket := fmt.Sprintf("%s-%s", ctx.Name, word)
-			url := fmt.Sprintf("https://%s.s3.amazonaws.com/", bucket)
-
-			result := CheckBucket(url, ctx.Debug)
-			
-			if result.Exist {
-				msg := fmt.Sprintf("[ACHEI] %s", url)
-				fmt.Println(msg)
-				if ctx.OutFile != nil {
-					fmt.Fprintln(ctx.OutFile, msg)
-				}
-				if ctx.StopOnFound {
-					close(ctx.Stop)
-					return
-				}
-			}
+		if !lastSep {
+			b.WriteByte('-')
+			lastSep = true
 		}
 	}
+
+	return strings.Trim(b.String(), "-.")
+}
+
+func isValidBucketName(name string) bool {
+	if len(name) < 3 || len(name) > 63 {
+		return false
+	}
+
+	for i, r := range name {
+		isLetter := r >= 'a' && r <= 'z'
+		isNumber := r >= '0' && r <= '9'
+		isSeparator := r == '.' || r == '-'
+		if !isLetter && !isNumber && !isSeparator {
+			return false
+		}
+
+		if (i == 0 || i == len(name)-1) && !isLetter && !isNumber {
+			return false
+		}
+	}
+
+	if strings.Contains(name, "..") {
+		return false
+	}
+
+	parts := strings.Split(name, ".")
+	if len(parts) == 4 {
+		looksLikeIP := true
+		for _, p := range parts {
+			n, err := strconv.Atoi(p)
+			if err != nil || n < 0 || n > 255 {
+				looksLikeIP = false
+				break
+			}
+		}
+		if looksLikeIP {
+			return false
+		}
+	}
+
+	return true
+}
+
+func generateVariants(name, word string) []string {
+	separators := []string{"-", ".", ""}
+    seen := make(map[string]struct{})
+    var variants []string
+
+    add := func(s string) {
+		s = sanitizeBucketPart(s)
+		if s == "" || !isValidBucketName(s) {
+			return
+		}
+        if _, ok := seen[s]; !ok {
+            seen[s] = struct{}{}
+            variants = append(variants, s)
+        }
+    }
+
+    for _, sep := range separators {
+        add(name + sep + word) 
+        add(word + sep + name) 
+    }
+
+    return variants
+} 
+
+func worker(wg *sync.WaitGroup, jobs <-chan string, ctx *BruteContext) {
+    defer wg.Done()
+    for word := range jobs {
+        select {
+        case <-ctx.Stop:
+            return
+        default:
+            variants := generateVariants(ctx.Name, word)
+			if len(variants) == 0 {
+				if ctx.Debug {
+					fmt.Printf("[DEBUG] ignorando entrada invalida da wordlist: %q\n", word)
+				}
+				continue
+			}
+            for _, bucket := range variants {
+                select {
+                case <-ctx.Stop:
+                    return
+                default:
+                    url := fmt.Sprintf("https://%s.s3.amazonaws.com/", bucket)
+                    result := CheckBucket(url, ctx.Debug)
+                    if result.Exist {
+                        msg := fmt.Sprintf("[ACHEI] %s", url)
+                        fmt.Println(msg)
+					
+                        if ctx.OutFile != nil {
+                            fmt.Fprintln(ctx.OutFile, msg)
+                        }
+				
+                        if ctx.StopOnFound {
+							ctx.StopOnce.Do(func() { close(ctx.Stop) })
+                            return
+                        }
+                    } else
+					{
+						msg := fmt.Sprintf("[NÃO ENCONTRADO] %s", url)
+						fmt.Println(msg)	
+					}
+				
+                }
+            }
+        }
+    }
 }
